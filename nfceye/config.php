f850663ec5519ad7840f1d433168ef71a721db86
cahitcanozgenn <cahitcanozgen.dev@gmail.com>
2022-06-03 10:25:36 +0300

    NfcEye

---
+++ nfceye/config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'nfceye';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
